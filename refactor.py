bfe1b791290bc3c7f4ee28b4f102d10f51b5e17c
Nick VanDette <nicholas.vandette@nuix.com>
2018-11-26 15:59:05 -0500

    progress on refactor

--- refactor.py
+++ refactor.py
@@ -2,9 +2,21 @@ import discord
 import json
 import re
 
+responses = {
+    "nouser": 'The specified user is not a user on this server.',
+    "!transfer": {
+        "usage": 'Please enter an amount followed by a mention to the recipient.\nex. `!transfer <amount> <mention>`',
+        "nofunds": 'You do not have enough beans to complete the transaction.',
+        "posamt": 'Please specify a positive transfer amount.',
+        "success": 'Transaction completed successfully. You now have {} beans in your account. {}'
+    }
+}
 
 class InvalidCommandException(Exception):
-    pass
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
 
 class Ledger:
     def __init__(self):
@@ -27,24 +39,34 @@ class Command:
         tokens = message.content.split(' ')
 
         # Baseline acceptance criteria
-        if len(validationCriteria) > len(tokens)-1:
+        if len(types) > len(tokens)-1:
             # Command must have at least len(criteria) tokens.
-            raise InvalidCommandException
+            raise InvalidCommandException(responses[tokens[0]]['usage'])
         
         # Parse into arguments.
-        self.arguments = []
+        self.args = []
         for index in range(1, len(types)):
             try:
-                if not criteria(tokens[index]):
-                    raise InvalidCommandException
-                else:
-                    self.arguments.append(types[index](tokens[index]))
-            except Exception:
-                raise InvalidCommandException
+                self.args.append(types[index](tokens[index]))
+            except:
+                raise InvalidCommandException(responses[tokens[0]]['usage'])
 
 async def send(location, message):
     global client
     await client.send_message(location, message)
+
+async def verifyValidUser(mention):
+    global client
+    userID = re.sub("<|>|@|!", "", mention)
+    try:
+        await client.get_user_info(userID)
+        return True
+    except discord.NotFound:
+        return False
+
+def usrid(mention):
+    userID = re.sub("<|>|@|!", "", str(mention))
+    return userID
 
 with open('../TOKEN') as tokenFile:
     TOKEN = tokenFile.readline()
@@ -65,19 +87,30 @@ async def on_message(message):
         try:
             # Command !balance takes no vaildation criteria,
             # and has no anticipated types.
-            command = Command(message, [], [])
+            command = Command(message, [])
             ledger.addNewUser(command.sender.id)
-            balance = ledger.data[command.sender.id]['balance'])
-            send(message.channel, 'Your balance is {} beans.'.format(balance))
+            balance = ledger.data[command.sender.id]['balance']
+            await send(message.channel, 'Your balance is {} beans.'.format(balance))
         except InvalidCommandException:
             # This command literally cannot be invalid
             pass
     
     if message.content.startswith('!transfer'):
         try:
-            validationCriteria = []
-            validationCriteria.append([int, ])
-            command = Command(message, validationCriteria, [])
+            command = Command(message, [int, usrid])
+            sender = command.sender.id
+            recipient = command.args[1]
+            amount = command.args[0]
+            ledger.addNewUser(command.sender.id)
+            if amount > ledger.data[sender]['balance']: raise InvalidCommandException(responses['!transfer']['nofunds'])
+            if amount > 0: raise InvalidCommandException(responses['!transfer']['posamt'])
+            if not await verifyValidUser(recipient): raise InvalidCommandException(responses['nouser'])
+            ledger.addNewUser(recipient)
+            ledger.data[sender]    -= amount
+            ledger.data[recipient] += amount
+            await send(message.channel, responses["success"].format(ledger.data[sender], "(Hey, thanks!)" if recipient == "512696929973698582" else ""))
+        except InvalidCommandException as e:
+            await send(message.channel, e.value)
 
 @client.event
 async def on_ready():
